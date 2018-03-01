547a9cf28db7ae0cfb4f436e9e003c253b14212f
JigarFotaria <jigarfotaria@gmail.com>
2018-03-01 18:42:22 +0530

    All changes. till now

--- js/commanjs.js
+++ js/commanjs.js
@@ -3,24 +3,29 @@
         $(this).toggleClass('color-red');
         return false;
     });
-    $('.bag').on('click', function(){
-        window.location = "cart.html"
-    });
-    $('.removeItem').on('click', function(){
-        $(this).parents('.itemRow').remove();
-    });
-    $('.owl-carousel').owlCarousel({
+
+    // $('.bag').on('click', function(){
+    //     window.location = "cart.html"
+    // });
+    // $('.removeItem').on('click', function(){
+    //     $(this).parents('.itemRow').remove();
+    // });
+    $('.ol1,.ol3').owlCarousel({
         loop:true,
         margin:10,
+        rewind: true,
         navText: ["<i class='fa fa-long-arrow-left' aria-hidden='true'></i>","<i class='fa fa-long-arrow-right' aria-hidden='true'></i>"],
         responsiveClass:true,
+        autoplay:true,
+        autoplayTimeout:2500,
+        autoplayHoverPause:false,
         responsive:{
             0:{
                 items:1,
                 nav:true
             },
             600:{
-                items:3,
+                items:2,
                 nav:true,
             },
             1000:{
@@ -29,7 +34,31 @@
                 loop:false
             }
         }
-    })
+    });
+    $('.ol2').owlCarousel({
+        loop:true,
+        margin:10,
+        rewind: true,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:false,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:false,
+                loop:false
+            }
+        }
+    });
 
 });
          
